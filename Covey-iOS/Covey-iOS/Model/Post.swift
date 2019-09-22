da18185ff0cbbf62972aa1d9ac1b6c687784afc9
KakaoTocs <kimjinu5555@gmail.com>
2019-09-22 17:35:46 +0900

    Add Model/User, Model/Post files for data schema defining

---
+++ Covey-iOS/Covey-iOS/Model/Post.swift
@@ -0,0 +1,22 @@
+//
+//  Post.swift
+//  Covey-iOS
+//
+//  Created by Jinu Kim on 2019/09/22.
+//  Copyright © 2019 com.KakaoTocs.kr. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: String
+    let title: String
+    let workingDate: String
+    let workingTime: String
+    let pay: String
+    let address: String
+    let userId: String
+    let dueDate: String
+    let description: String
+    let category: String
+}
---
+++ Covey-iOS/Covey-iOS/Model/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  Covey-iOS
+//
+//  Created by Jinu Kim on 2019/09/22.
+//  Copyright © 2019 com.KakaoTocs.kr. All rights reserved.
+//
+
+import Foundation
+
+struct User {
+    
+    let id: String
+    let name: String
+    let email: String
+    let createdAt: String
+    let updatedAt: String
+    let deletedAt: String
+    let nickname: String
+    let address: String
+    let univ: String
+    let gender: String
+    let age: Int
+    let career: String
+    let phoneNumber: String
+}
