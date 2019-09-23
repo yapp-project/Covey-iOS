cb8422efd7f2fc674d5fa853e917bf47fb1b1aeb
KakaoTocs <kimjinu5555@gmail.com>
2019-09-23 23:22:34 +0900

    Add Resource/ColorManager file for color management

---
+++ Covey-iOS/Covey-iOS/Resource/ColorManager.swift
@@ -0,0 +1,16 @@
+//
+//  ColorManager.swift
+//  Covey-iOS
+//
+//  Created by Jinu Kim on 2019/09/22.
+//  Copyright © 2019 com.KakaoTocs.kr. All rights reserved.
+//
+
+import UIKit
+
+struct ColorManager {
+    
+    static let tomato = #colorLiteral(red: 0.9490196078, green: 0.2509803922, blue: 0.2509803922, alpha: 1)
+    static let easterPurple = #colorLiteral(red: 0.6588235294, green: 0.4705882353, blue: 1, alpha: 1)
+    static let silver = #colorLiteral(red: 0.7843137255, green: 0.7882352941, blue: 0.7921568627, alpha: 1)
+}
