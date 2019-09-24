4472553cda9ff1a2c59093ac9ca473e316a6c187
KakaoTocs <kimjinu5555@gmail.com>
2019-09-24 23:58:17 +0900

    Add CustomComponent/ActiveButton file for apply default Done button style

---
+++ Covey-iOS/Covey-iOS/CustomComponent/ActiveButton/ActiveButton.swift
@@ -0,0 +1,13 @@
+//
+//  ActiveButton.swift
+//  Covey-iOS
+//
+//  Created by Jinu Kim on 2019/09/24.
+//  Copyright © 2019 com.KakaoTocs.kr. All rights reserved.
+//
+
+import UIKit
+
+class ActiveButton: UIButton {
+    
+}
