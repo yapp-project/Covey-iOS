4e310c9a3eb7e54df937287db9c289027090e0be
KakaoTocs <kimjinu5555@gmail.com>
2019-09-26 23:57:49 +0900

    Add SNSLoginButton

---
+++ Covey-iOS/Covey-iOS/CustomComponent/SNSLoginButton/SNSLoginButton.swift
@@ -0,0 +1,13 @@
+//
+//  SNSLoginButton.swift
+//  Covey-iOS
+//
+//  Created by Jinu Kim on 2019/09/26.
+//  Copyright © 2019 com.KakaoTocs.kr. All rights reserved.
+//
+
+import UIKit
+
+class SNSLoginBUtton: UIButton {
+    
+}
