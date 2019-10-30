6ba12af3ee491bca9a4f398ec48783a99884e860
GAMZA <jinu.kim@platfarm.net>
2019-10-31 00:04:11 +0900

    Add model schema

---
+++ Covey-iOS/Covey-iOS/Model/Post.swift
@@ -0,0 +1,35 @@
+//
+//  Post.swift
+//  Covey-iOS
+//
+//  Created by Jinu Kim on 2019/09/22.
+//  Copyright © 2019 com.KakaoTocs.kr. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let id: Int
+    let userID: Int
+    
+    let title: String
+    
+    let startDate: String
+    let endDate: String
+    let dueDate: String
+    
+    let address1: String
+    let address2: String
+    let address3: String
+    
+    let pay: Int
+    let description: String
+    let category: String
+    
+    let workingDate: String
+    let workingTime: String
+}
+
+extension Post {
+    
+}
